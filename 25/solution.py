db6f92fefb63a915d5b1da14072e83c3fcf2fa32
dante7867 <dante7867@gmail.com>
2022-12-25 19:15:42 +0100

    Add solution for day 25

--- 25/solution.py
+++ 25/solution.py
@@ -1,59 +1,48 @@
 #!/usr/bin/env python3
 # https://adventofcode.com/2022/day/25
 
+
 from copy import deepcopy
-import itertools
-
-inp = []
-with open('i.txt', 'r') as f:
-    for line in f.readlines():
-        inp.append(line.strip())
-
-
-sanfu_symbol2digit={'2':2,'1':1,'0':0,'-':-1,'=':-2}
 
 
 def snafu2decimal(snafu_num):
     decimal=0
     mult = 5**(len(snafu_num)-1)
     for snafu_symbol in snafu_num:
-        digit = sanfu_symbol2digit[snafu_symbol]
+        digit = SNAFU_2_DIGITAL[snafu_symbol]
         decimal += mult * digit
         mult = mult//5
-    #print(snafu_num, decimal)
     return decimal
 
+
+inp = []
+with open('i.txt', 'r') as f:
+    for line in f.readlines():
+        inp.append(line.strip())
+
+SNAFU_2_DIGITAL={'2':2,'1':1,'0':0,'-':-1,'=':-2}
 
 s = 0
 for snafu_num in inp:
     s += snafu2decimal(snafu_num)
+print('sum:', s)
 
-print('sum', s)
-TAR = s
-N = ['2','1','0','-','=']
-ps = deepcopy(N)
-def snafu_with_len(n):
-    N = ['2','1','0','-','=']
 p = 0
 while 5**p < s:
     p+=1
-print(p, 5**p)
 
+start = '2'*p
+start = list(start)
+SNAFU_SYMBOLS = ['2','1','0','-','=']
+for idx in range(1, len(start)):
+    cp = deepcopy(start)
+    prev = 2
+    for sym in SNAFU_SYMBOLS:
+        cp[idx] = sym
+        diff = snafu2decimal(cp) - s
+        if diff < 0:
+            break
+        prev = sym 
+    start[idx] = prev
 
-
-
-"""
-#better
-l=0
-while True:
-    l += 1
-    print(f'considering len of number = {l}')
-    for p in itertools.product(N, repeat=l):
-        if snafu2decimal(p) == TAR:
-            print(''.join(p))
-            exit()
-
-"""
-
-
-
+print('P1:', ''.join(start))
